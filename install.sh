dbb783c147e8d14390939515cb465970d1f5853e
Fyk1ll <wilson.marquesfykill@protonmail.com>
2017-05-14 17:00:09 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,12 +6,6 @@ if [ $USER != "root" ]; then
 	echo "Nao permitido, entre como root";
 	exit
 fi
-
-if [ -e "/opt/md5brute/" ]
-then
-	chmod +x md5brute.php
-        ln -s /opt/md5brute/md5brute.php /usr/bin/md5brute
-        echo -ne "$color A instalaçao terminou! Utilize o comando md5brute\n";
-else
-	echo -ne  "Mova o md5brute para /opt \n";
-fi
+chmod +x md5brute.php
+ln -s /opt/md5brute/md5brute.php /usr/bin/md5brute
+echo -ne "$color A instalaçao terminou! Utilize o comando md5brute\n";
